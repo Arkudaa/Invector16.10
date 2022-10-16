55a905821a3083484e67c1faa175662a995e14dc
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2022-10-16 13:07:19 +0300

    vers01

---
+++ JuriSarki/Invector/Assets/Scripts/PickUp.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+    public AudioSource coinmusic;
+    public int score;
+    //public Text scoreText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "coin")
+        {
+            Destroy(collision.gameObject);
+            score++;
+            //scoreText.text = "Score: " + score;
+            coinmusic.Play();
+            
+        }
+
+        if (collision.gameObject.tag == "danger")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        
+    }
+}
